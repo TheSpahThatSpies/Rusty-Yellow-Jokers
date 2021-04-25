28b15b99b9a821af552ac4d918173cd3584fc28a
TheSpahThatSpies <christianborggaard@gmail.com>
2021-04-25 11:19:30 +0200

    Revert "Loading - Desktop"
    This reverts commit 6184aa66a3c5053a4e266c4239022df2c8ed1f4c.

--- load.js
+++ load.js
@@ -1,7 +1,3 @@
 //var timer = setTimeout(function() {
 //    window.location='http://example.com'
-//}, 8200);
-
-timer = setTimeout(function(){
-      $(".img-droplet").show()
-}, 7600);
+//}, 8000);
